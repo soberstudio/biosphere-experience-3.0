f3bcabe150d92b6ce71a0f16a841cf165fd5afd2
soberstudio <hello@soberstudio.dev>
2025-10-06 15:10:45 +0800

    Robots + Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://biosphere-experience.com', // Your production URL
+  integrations: [sitemap()],
   devToolbar: {
     enabled: false
   },
