08e8bbf14cfabc8b2acd93fb7644b9c8757fa529
soberstudio <hello@soberstudio.dev>
2025-10-06 15:25:10 +0800

    description

--- src/data/SiteData.ts
+++ src/data/SiteData.ts
@@ -1,7 +1,7 @@
 export const siteInfo = {
   name: "BIOSPHERE EXPERIENCE",
   url: import.meta.env.SITE || "https://biosphere-experience.org/",
-  description: "Notre objectif est d'explorer un nouveau futur dans lequel les humains interagissent avec d'autres espèces vivantes et les éléments. Nous concevons et expérimentons des modes de vie plus justes, sains, durables et conviviaux.",
+  description: "Expérimenter un futur durable et désirable dans lequel les humains vivent en symbiose avec les autres espèces vivantes.",
   keywords: "low-tech, durabilité, innovation, autonomie, vie urbaine, économie circulaire, réduction des déchets, énergies renouvelables, science participative, mode de vie écologique",
   author: "Biosphère Experience",
   email: "contact@biosphere-experience.org",
