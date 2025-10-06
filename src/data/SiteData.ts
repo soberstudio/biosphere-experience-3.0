a6c6827fca4450bcab8405ad59b7cb6781bc143a
soberstudio <hello@soberstudio.dev>
2025-10-06 14:57:42 +0800

    Initial commit

---
+++ src/data/SiteData.ts
@@ -0,0 +1,14 @@
+export const siteInfo = {
+  name: "Biosphere Experience",
+  url: "https://biosphere-experience.org/",
+  description: "Il existe des technologies et savoir-faire innovants, accessibles à tous et durables pour répondre à nos besoins.",
+  keywords: "low-tech, durabilité, innovation, autonomie, vie urbaine, économie circulaire, réduction des déchets, énergies renouvelables, science participative, mode de vie écologique",
+  author: "Biosphère Experience",
+  email: "contact@biosphere-experience.org",
+  social: {
+    youtube: "https://www.youtube.com/@BiosphereExperience",
+    linkedin: "https://www.linkedin.com/company/biosphere-experience",
+    instagram: "https://www.instagram.com/biosphere.experience",
+  },
+ 
+}
