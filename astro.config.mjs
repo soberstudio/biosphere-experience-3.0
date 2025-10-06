b32178925193cf59b5844c5aa352a8b5065c738b
soberstudio <hello@soberstudio.dev>
2025-10-06 19:36:31 +0800

    equipe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://biosphere-experience.org', // Your production URL
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
   devToolbar: {
     enabled: false
   },
